7c513e39c69915203bc0e367a590663bb0b3e2c5
Oleg Nepiuk <gtailde@gmail.com>
2025-06-14 22:02:25 +0200

    add: basic game functionality

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,68 +1,179 @@
 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
 class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  constructor() {
+    this.state = this.getState();
+    this.gameStatus = 'idle';
+    this.score = 0;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  updateScore(newScore = 0) {
+    this.score += newScore;
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
+    const gameScore = document.querySelector('.game-score');
 
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
+    gameScore.innerText = String(this.score);
+  }
 
-  /**
-   * Returns the current game status.
-   *
-   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
-   *
-   * `idle` - the game has not started yet (the initial state);
-   * `playing` - the game is in progress;
-   * `win` - the game is won;
-   * `lose` - the game is lost
-   */
-  getStatus() {}
+  getState() {
+    const fieldRows = document.querySelectorAll('.field-row');
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+    return Array.from(fieldRows).map((row) => {
+      return Array.from(row.children).map((cell) => {
+        const cellContent = cell.textContent.trim();
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+        return isNaN(+cellContent) ? 0 : +cellContent;
+      });
+    });
+  }
 
-  // Add your own methods here
+  messageController(msgClassName = null) {
+    document.querySelectorAll('.message').forEach((el) => {
+      if (msgClassName && el.classList.contains(msgClassName)) {
+        el.classList.remove('hidden');
+      } else {
+        el.classList.add('hidden');
+      }
+    });
+  }
+
+  start() {
+    this.reset();
+    this.messageController();
+    this.gameStatus = 'playing';
+    this.addNewCell();
+    this.updateField();
+  }
+
+  reset() {
+    this.score = 0;
+    this.updateScore();
+    this.gameStatus = 'idle';
+    this.messageController('message-start');
+    this.updateField(true);
+    this.state = this.getState();
+  }
+
+  addNewCell() {
+    const newCellValue = Math.random() < 0.9 ? 2 : 4;
+    const getEmptyCells = () => {
+      return this.state.reduce((emptyCellsArr, row, rowIndex) => {
+        row.forEach((cell, cellIndex) => {
+          if (cell === 0) {
+            emptyCellsArr.push([rowIndex, cellIndex]);
+          }
+        });
+
+        return emptyCellsArr;
+      }, []);
+    };
+    const emptyCells = getEmptyCells();
+
+    if (!emptyCells.length) {
+      this.gameStatus = 'lose';
+      this.messageController('message-lose');
+
+      return;
+    }
+
+    const randomCell =
+      emptyCells[Math.floor(Math.random() * emptyCells.length)];
+
+    this.state[randomCell[0]][randomCell[1]] = newCellValue;
+  }
+
+  updateState(reverse = false, transform = false) {
+    const transformField = (field) => {
+      return field[0].map((_, colIndex) => field.map((row) => row[colIndex]));
+    };
+
+    let scoreCounter = 0;
+    let stateCopy = this.state.map((row) => [...row]);
+
+    if (transform) {
+      stateCopy = transformField(stateCopy);
+    }
+
+    stateCopy = stateCopy.map((row) => {
+      const filteredRow = [...row].filter((cell) => cell !== 0);
+
+      if (reverse) {
+        filteredRow.reverse();
+      }
+
+      for (let col = filteredRow.length - 1; col > 0; col--) {
+        if (filteredRow[col] === filteredRow[col - 1]) {
+          scoreCounter += filteredRow[col] * 2;
+          filteredRow[col] *= 2;
+          filteredRow.splice(col - 1, 1);
+          col--;
+        }
+      }
+
+      while (filteredRow.length < this.state[0].length) {
+        filteredRow.unshift(0);
+      }
+
+      if (reverse) {
+        filteredRow.reverse();
+      }
+
+      return filteredRow;
+    });
+
+    if (transform) {
+      stateCopy = transformField(stateCopy);
+    }
+
+    this.state = stateCopy;
+    this.addNewCell();
+    this.updateField();
+    this.updateScore(scoreCounter);
+  }
+
+  moveUp() {
+    this.updateState(true, true);
+  }
+
+  moveDown() {
+    this.updateState(false, true);
+  }
+
+  moveRight() {
+    this.updateState();
+  }
+
+  moveLeft() {
+    this.updateState(true);
+  }
+
+  updateField(reset = false) {
+    const fieldRows = document.querySelectorAll('.field-row');
+
+    fieldRows.forEach((row, rowIndex) => {
+      Array.from(row.children).forEach((cell, colIndex) => {
+        let cellValue;
+
+        if (!reset) {
+          cellValue = this.state[rowIndex][colIndex];
+        }
+
+        cell.className = 'field-cell';
+
+        if (cellValue !== 0 && !reset) {
+          cell.innerText = cellValue;
+          cell.classList.add(`field-cell--${cellValue}`);
+        } else {
+          cell.innerText = '';
+        }
+      });
+    });
+
+    if (this.state.flat(Infinity).includes(2128)) {
+      this.gameStatus = 'win';
+      this.messageController('message-win');
+    }
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,39 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+window.addEventListener('keydown', (e) => {
+  if (game.gameStatus === 'playing') {
+    switch (e.key) {
+      case 'ArrowUp':
+        game.moveUp();
+        break;
+      case 'ArrowDown':
+        game.moveDown();
+        break;
+      case 'ArrowRight':
+        game.moveRight();
+        break;
+      case 'ArrowLeft':
+        game.moveLeft();
+        break;
+      default:
+    }
+  }
+});
+
+const button = document.querySelector('.button');
+
+button.addEventListener('click', () => {
+  button.classList.toggle('start');
+  button.classList.toggle('reset');
+
+  if (button.innerText === 'Start') {
+    button.innerText = 'Reset';
+    game.start();
+  } else {
+    button.innerText = 'Start';
+    game.reset();
+  }
+});
